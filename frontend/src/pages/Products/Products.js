ee0454e0a4cd32b2fa8eecaa1e2ef2176be3caf9
Aleksander Piron <42944811+aleksanderpiron@users.noreply.github.com>
2019-11-18 00:58:35 +0100

    ss

--- frontend/src/pages/Products/Products.js
+++ frontend/src/pages/Products/Products.js
@@ -3,30 +3,14 @@ import ProductsList from './ProductsList';
 import AddProductForm from '../../components/Forms/AddProductForm';
 import ReactCSSTransitionGroup  from 'react-addons-css-transition-group';
 
-class Products extends Component{
-    state={
-        pages:{
-            currentPage:'productsList',
-            productsList:true,
-            addProduct:false
-        }
-    }
-    switchPageTo=(target)=>{
-        this.setState({
-            pages.currentPage:target
-        })
-        
-    }
-    render(){
-        console.log(this.state.pages);
-        return(
-            <ReactCSSTransitionGroup component='div' className="page products" timeout={500} transitionName="notif">
-                {this.state.pages.productsList && <ProductsList />}
-                {this.state.pages.addProduct && <AddProductForm />}
-                <button onClick={()=>{this.switchPageTo('addProduct')}}>Switch</button>
-            </ReactCSSTransitionGroup>
-       )
-    }
+const Products=()=>{
+     return(
+         <div className="page products" timeout={500} transitionName="notif">
+             {this.state.pages.productsList && <ProductsList />}
+             {this.state.pages.addProduct && <AddProductForm />}
+             <button onClick={()=>{this.switchPageTo('addProduct')}}>Switch</button>
+         </div>
+    )
 }
 
 export default Products;
